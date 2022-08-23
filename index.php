ae14604659a2b8b218db4b29848c77d74d9c82eb
stecarraa <stefano@caratozzolo.me>
2022-08-23 09:53:13 +0200

    fix: error display

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 // frase da censurare
 $myPhrase = 'Figli di Gondor! Di Rohan! Fratelli miei! Vedo nei vostri occhi la stessa paura che potrebbe afferrare il mio cuore! Ci sarà un giorno, in cui il coraggio degli uomini cederà, in cui abbandoneremo gli amici e spezzeremo ogni legame di fratellanza, ma non è questo il giorno! Ci sarà l\'ora dei lupi e degli scudi frantumati quando l\'era degli uomini arriverà al crollo, ma non è questo il giorno! Quest\'oggi combattiamo! Per tutto ciò che ritenete caro su questa bella terra, v\'invito a resistere! Uomini dell\'Ovest!';
 // sostituzione
-$blip='***';
+$blip ='***';
 // parola a scelta da censurare
-$badWord= $_GET['word'];
+$badWords = $_GET['badword'];
 
 // frase post censura con sostituzione
-$myCensoredPhrase = str_replace($badWord, $blip, $myPhrase)
+$myCensoredPhrase = str_replace($_GET['badword'], $blip, $myPhrase)
 
 ?>
 
